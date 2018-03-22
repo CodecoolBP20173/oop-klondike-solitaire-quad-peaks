4ac624fd3d694018308b53d00caa3815fb960083
Dominik Hug <h.dom90@gmail.com>
2018-03-22 09:26:05 +0100

    Fixed keywords

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -9,6 +9,7 @@ public class Klondike extends Application {
 
     private static final double WINDOW_WIDTH = 1400;
     private static final double WINDOW_HEIGHT = 900;
+    public static Game game;
 
     public static void main(String[] args) {
         launch(args);
@@ -21,7 +22,7 @@ public class Klondike extends Application {
 
     public static void startGame(Stage primaryStage) {
         Card.loadCardImages();
-        Game game = new Game(primaryStage);
+        game = new Game(primaryStage);
         game.setTableBackground(new Image("/table/green.png"));
 
         primaryStage.setTitle("Klondike Solitaire");
